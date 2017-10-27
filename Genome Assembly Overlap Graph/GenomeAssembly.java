c6091b9b6ac2649b52cb016dcaab8952c3009c35
Navjinder Virdee <navjndervirdee@gmail.com>
2017-10-27 16:40:17 +0530

    Cleaned the code and added comments.

--- Genome Assembly Overlap Graph/GenomeAssembly.java
+++ Genome Assembly Overlap Graph/GenomeAssembly.java
@@ -1,3 +1,5 @@
+//Program to Assemble the Phi-X174 genome using Overlap Graph.
+
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.util.StringTokenizer;
 
 
 public class GenomeAssembly{
+	//function to get the input reads of the genome.
 	public static Vertex [] getReads(){
 		FastReader s=new FastReader();
 		Vertex [] graph = new Vertex[1618];
@@ -17,11 +20,13 @@ public class GenomeAssembly{
 		return graph;
 	}
 
+
+	//class for a Vertex of a Graph.
 	static class Vertex{
-		int vertexNum;
-		String read;
-		Map<Integer,Integer> edges; //could remove duplicated strings. if not passed then can use list instead of map.
-		boolean found;
+		int vertexNum;			//id of the vertex.
+		String read;			//read of the vertex.
+		Map<Integer,Integer> edges; 	//Keys are indexes of adjacent vertices, and Values are length of overlap between the two strings (read).
+		boolean found;			//if found while traversing the graph.
 		
 		public Vertex(int vertexNum, String read){
 			this.vertexNum = vertexNum;
@@ -31,6 +36,7 @@ public class GenomeAssembly{
 		}
 	}
 	
+	//class for reading the input.
 	static class FastReader{
         	BufferedReader br;
         	StringTokenizer st;
@@ -39,108 +45,87 @@ public class GenomeAssembly{
             		br = new BufferedReader(new InputStreamReader(System.in));
         	}
  
-        	String next(){
-           	   while (st == null || !st.hasMoreElements()){
-            	     try{
-                	    st = new StringTokenizer(br.readLine());
-                     }
-	             catch (IOException  e){
-        	            e.printStackTrace();
-               	     }
-                   }
-                   return st.nextToken();
+		String next(){
+			while (st == null || !st.hasMoreElements()){
+            	     		try{
+            				st = new StringTokenizer(br.readLine());
+                     		}
+	             		catch (IOException  e){
+        	       	     		e.printStackTrace();
+               	     		}
+                   	}
+                   	return st.nextToken();
+		}
+ 
+ 	       	int nextInt(){
+        	    	return Integer.parseInt(next());
+               	}
+ 
+	       	long nextLong(){
+        	   	return Long.parseLong(next());
+               	}
+ 
+              	double nextDouble(){
+                   	return Double.parseDouble(next());
+              	}
+ 
+        	String nextLine(){
+            		String str = "";
+            		try{
+	                	str = br.readLine();
+        	    	}
+            		catch (IOException e){
+               	 		e.printStackTrace();
+            		}
+            		return str;
         	}
- 
- 	       int nextInt(){
-        	    return Integer.parseInt(next());
-               }
- 
-	       long nextLong(){
-        	   return Long.parseLong(next());
-               }
- 
-              double nextDouble(){
-                   return Double.parseDouble(next());
-              }
- 
-        String nextLine(){
-            String str = "";
-            try
-            {
-                str = br.readLine();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
-
+    	}
+	
+	//function to make an Overlap graph.
 	private static void makeOverlapGraph(Vertex [] graph){
 		for(int i=0; i<graph.length; i++){
 			for(int j=0; j<graph.length; j++){
-					if(j==i){
-						continue;
-					}
-					char [] str1 = graph[i].read.toCharArray();
-					char [] str2 = graph[j].read.toCharArray();
-					for(int k=0; k<str2.length; k++){
-						int length = str1.length-k;
-						int error = (int)(length*0.03);
-						//System.out.println(str1.substring(k) + " " + str2.substring(0,length) + " " + str1.substring(k).equals(str2.substring(0,length)));
-						
-						
-						boolean overlap = true;
-						int m = 0;
-						for(int l=k; l<str1.length;l++){
-							if(error==0){
-								overlap=false;
-								break;
-							}
-							if(str1[l]!=str2[m]){
-								//System.out.println(i + " " + j + " " + k + " " +  str1[l] + " " + str2[m]);
-								error--;			
-							}
-							//System.out.println(str1[l] + " " + str2[m]);
-							m++;
-						}
-						
-						if(overlap){
-							graph[i].edges.put(j,length);
+				if(j==i){
+					continue;
+				}
+				char [] str1 = graph[i].read.toCharArray();
+				char [] str2 = graph[j].read.toCharArray();
+				for(int k=0; k<str2.length; k++){
+					int length = str1.length-k;
+					int error = (int)(length*0.03);   	//considering an error of only 3% in the overlap between the reads.
+					
+					boolean overlap = true;
+					int m = 0;
+					for(int l=k; l<str1.length;l++){
+						if(error==0){
+							overlap=false;
 							break;
 						}
-							
-								
-						/*if(str1.substring(k).equals(str2.substring(0,length))){
-							graph[i].edges.put(j,length);
-							break;
-						}*/
+						if(str1[l]!=str2[m]){
+							error--;			
+						}
+						m++;
 					}
+						
+					if(overlap){
+						graph[i].edges.put(j,length);
+						break;
+					}
+				}
 			}
 		}
-	
-		/*for(int i=0;i<graph.length;i++){
-			System.out.println(graph[i].edges);
-		}*/
 	}
 
-	static int last = -1;		
-				
+	static int last = -1;	//store the index of the last explored vertex.
+
+	//function to find hamiltonian path in the graph using a greedy approach. i.e choosing the next vertex with max overlap length.				
 	private static String findHamiltonianPath(Vertex [] graph){
 		String genome = "";
-		int random = (int)(Math.random()*(graph.length-2) + 1);
-		//System.out.println(5);
+		int random = (int)(Math.random()*(graph.length-2) + 1); 	//starting with randomly picked vertex.
 		int first = random;
-		
-		//System.out.println(first);
 		
 		genome = genome + graph[random].read;
 		genome = explore(graph,random,genome);
-
-		//System.out.println(last);
-		
-		//System.out.println(genome);
 		
 		for(int i=0;i<graph.length;i++){
 			if(!graph[i].found){
@@ -157,44 +142,33 @@ public class GenomeAssembly{
 		return genome;
 	}
 	
+	//function to explore the given vertex index.
 	private static String explore(Vertex [] graph, int random, String genome){
 		graph[random].found = true;
 		int max = -1;
 		int index = -1;
 		for(int key : graph[random].edges.keySet()){
 			int temp = graph[random].edges.get(key);
-			//System.out.println(temp);
 			if(graph[key].found==false && max<temp){
 				max = temp;
 				index = key;
 			}
 		}
-		//System.out.println(max + " " + index);
+		
 		if(index==-1){
 			return genome;
 		}
+
 		genome = genome + graph[index].read.substring(max);
 		last = index;
-		//System.out.println(genome + " " + graph[index].read.substring(max));
 		return explore(graph,index,genome);
-		
 	}
 
+	//main function to run the program.
 	public static void main(String [] args){
-		Vertex [] graph = getReads();
-		
-		makeOverlapGraph(graph);
-		String genome = findHamiltonianPath(graph);
-		System.out.println(genome);
+		Vertex [] graph = getReads();  			//get the reads.
+		makeOverlapGraph(graph);			//make overlap graph.
+		String genome = findHamiltonianPath(graph);	//find hamiltonian path.
+		System.out.println(genome);			//print the genome.
 	}
-}
-			
-		
-		
-		
-		
-		
-					
-					
-		
-			
+}			
